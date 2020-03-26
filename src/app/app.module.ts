1537d5717b12ca6a07d9f23432e3cd33f5ae5b47
Javier <javierfp1993@gmail.com>
2020-03-26 09:16:16 +0100

    FRONT, head, footer, perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { HeadComponent } from './componentes/head/head.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PeticionesComponent } from './componentes/peticiones/peticiones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent,
+    HeadComponent,
+    FooterComponent,
+    PeticionesComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
