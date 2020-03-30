7fe6edc7fb6fff07bd3dfcdf3d455eaa95093dc8
Jorge Rodriguez <jorger01@hotmail.es>
2020-03-30 18:53:34 +0200

    Subiendo servicios y clases

---
+++ src/app/models/peticiones.ts
@@ -0,0 +1,7 @@
+export class Peticiones {
+
+    constructor(public id_peticion: number, public categoria:String, public foto: String, public titulo:String, public precio:number, public localizacion: String, public descripcion: String, public fecha_finalizacion: Date, public estado: String){
+
+        
+    }
+}
---
+++ src/app/models/usuarios.ts
@@ -0,0 +1,7 @@
+export class Usuarios {
+
+    constructor(public id_usuario:number, public nombre_usuario: String, public nombre: String, public primer_apellido: String, public segundo_apellido: String, public telefono: number, public email:String, public contraseña:String, public biografia: String, public foto: String, public valoracion: number, public tareas_publicadas: number, public tareas_solicitadas: number, public monedas: number){
+
+        
+    }
+}
