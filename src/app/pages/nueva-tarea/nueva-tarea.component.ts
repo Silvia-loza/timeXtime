85754b31c389c5fc7dd0e8c028418ef788b62747
Jesus <jesusmontes1987@gmail.com>
2020-04-02 18:03:19 +0200

    Cambios Jesus

--- src/app/pages/nueva-tarea/nueva-tarea.component.ts
+++ src/app/pages/nueva-tarea/nueva-tarea.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NuevatareaService } from 'src/app/shared/nuevatarea.service';
+import { Peticiones } from 'src/app/models/peticiones';
 
 @Component({
   selector: 'app-nueva-tarea',
@@ -6,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nueva-tarea.component.css']
 })
 export class NuevaTareaComponent implements OnInit {
+  public categoria: String
+  peticiones: Object;
+  constructor(private apiService: NuevatareaService) { }
 
-  constructor() { }
+categorias(categoria:String){
+    this.categoria = categoria
+  }
+
+
+insertarTarea(foto:String, titulo:String, localizacion: String, precio:number, fecha_finalizacion: Date, descripcion: String)
+  { 
+    let peticion = new Peticiones()
+
+    peticion.categoria=this.categoria
+    peticion.foto=foto
+    peticion.titulo=titulo
+    peticion.localizacion=localizacion
+    peticion.precio=precio
+    peticion.fecha_finalizacion=fecha_finalizacion
+    peticion.descripcion=descripcion
+
+    this.apiService.postTarea(peticion).subscribe((data) =>
+  {
+    this.peticiones = data
+    //console.log(data)
+    })
+  }
+
+
+
+
+
 
   ngOnInit(): void {
   }
--- src/app/shared/nuevatarea.service.ts
+++ src/app/shared/nuevatarea.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Peticiones } from '../models/peticiones';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NuevatareaService {
+  private url = "http://localhost:3000/nueva-peticion/confirmar"
+  peticion:Peticiones
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+postTarea(nuevaTarea: Peticiones)
+{
+  return this.http.post(this.url, nuevaTarea )
+}
+
+
+
 }
