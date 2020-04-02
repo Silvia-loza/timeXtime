24c0d2e2cf98bb252fbb1cbabe187981230e9387
Silvia Lozano <sylvilozano@gmail.com>
2020-04-02 17:53:09 +0200

    cambios perfil silvia

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,13 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuarios} from '../../models/usuarios';
+import { PerfilService} from '../../shared/perfil.service'
+
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+  styleUrls: ['./perfil.component.css'],
+  providers:  [ PerfilService ]
 })
 export class PerfilComponent implements OnInit {
+  public usuario: Usuarios
+  public perfil : Object
+  constructor(private apiService: PerfilService) { }
 
-  constructor() { }
+  editarPerfil(id_usuario: number, nombre_usuario: String, nombre: String, primer_apellido: String, segundo_apellido: String,  contrasena: String, telefono: number, email: String, biografia: String, foto: String)
+   {
+    let nuevoPerfil = new Usuarios();
+    nuevoPerfil.id_usuario = id_usuario;
+    nuevoPerfil.nombre_usuario = nombre_usuario;
+    nuevoPerfil.nombre = nombre;
+    nuevoPerfil.primer_apellido= primer_apellido;
+    nuevoPerfil.segundo_apellido = segundo_apellido;
+    nuevoPerfil.contrasena = contrasena;
+    nuevoPerfil.telefono = telefono;
+    nuevoPerfil.email = email;
+    nuevoPerfil.biografia = biografia;
+    nuevoPerfil.foto = foto;
+
+    this.apiService.putPefil(nuevoPerfil).subscribe((data) =>
+    {
+      console.log(data)
+    })
+  }
+
+  mostrarPerfil(id_usuario: number)
+  {
+   
+
+    this.apiService.getPerfil(id_usuario).subscribe((data) =>
+    {
+      this.perfil = data;
+    })
+  }
+  onFotoSelected(event) {
+        console.log(event)
+  }
 
   ngOnInit(): void {
   }
--- src/app/shared/perfil.service.ts
+++ src/app/shared/perfil.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Usuarios} from '../../app/models/usuarios';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
+  private url= "http://localhost:3000/perfil/guardar"
+  private url2= "http://localhost:3000/header/perfil"
+  constructor(private http: HttpClient) { }
+  
+  putPefil(newUsuario: Usuarios){
+  return this.http.put(this.url, newUsuario)
+};
 
-  constructor() { }
+
+ getPerfil(id_usuario: number)
+ {
+   return this.http.get(this.url2 + "?id=" + id_usuario)
+ }
 }
